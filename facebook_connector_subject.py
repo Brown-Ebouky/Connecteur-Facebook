9195c44f0e9d362c505600d4460fb6e4c861b497
Brown-Ebouky <eboukybrown@gmail.com>
2021-05-18 20:04:31 +0200

    code avec facebook scraper

--- facebook_connector_subject.py
+++ facebook_connector_subject.py
@@ -3,23 +3,24 @@
 !pip install facebook-scraper
 
 # On fait les imports
-import urllib3, facebook, requests
+import facebook
 from facebook_scraper import get_posts
 import pymongo
 from pymongo import MongoClient
-import json
 
+
+# Token utilisateur pour l'application Facebook mise sur pied
+PAGE_TOKEN = "--------"
 
 """
-Cette fonction permet de récupérer des données relatives à un sujet "topic" dans une page "page"
+Cette fonction permet de récupérer des posts relatifs à un sujet "topic" dans une page "page"
 Les données récupérées sont le message du poste, les commentaires, et les probables photos
-
+- utilise l'api Facebook Graph
 Cette fonction renvoie la liste des posts qui correspondants au critère.
 
 """
 def main(topic = "le décès du président Jacques Chirac", page):
-	page_token = "--------"
-	graph = facebook.GraphAPI(access_token = page_token, version="10.0")
+	graph = facebook.GraphAPI(access_token = PAGE_TOKEN, version="10.0")
 	pages_data = graph.get_object(page)
 	page_id = pages_data["data"]["id"]
 
@@ -34,7 +35,7 @@ def main(topic = "le décès du président Jacques Chirac", page):
 		data_post = graph.get_object(id=post['id'], fields="""message,
                                       comments, picture""")
 
-		if data_post['message'].contains(topic):
+		if topic in data_post['message']:
 			post["data_post"] = data_post
 			real_posts.append(post)
 
@@ -42,13 +43,29 @@ def main(topic = "le décès du président Jacques Chirac", page):
 
 
 """
+Cette fonction permet de récupérer des posts relatifs à un sujet "topic" dans une page "page". 
+Le message du poste, les commentaires, et les probables photos sont contenues dans les données récupérées.
+
+- utilise la librairie Facebook-scraper
+
+Cette fonction renvoie la liste des posts qui correspondants au critère.
+"""
+def post_scrapper(topic = "le décès du président Jacques Chirac", page):
+	listposts = []
+	for post in get_posts(page, pages=20, options={"comments": True}):
+    	if topic in post['text']: 
+        	listposts.append(post)
+    return listposts
+
+
+"""
 On utilise la méthode main pour prendre les posts dans la page "kaisens" qui parlent du 
 décès de chirac puis on insère dans la base de données.
 """
-
 if __name__ == '__main__':
 	
-	final_posts = main(page="kaisens")
+	# final_posts = main(page="kaisens")
+	final_posts = post_scrapper(page="kaisens")
 	
 	try:
 		conn = MongoClient()
